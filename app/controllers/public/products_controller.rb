3fd9241c10f3306efe7b8b1b1e68f4b84ce3122a
vagrant <vagrant@localhost.localdomain>
2020-08-18 12:57:49 +0000

    [Update] public/product

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,7 +1,13 @@
 class Public::ProductsController < ApplicationController
   def show
+  	@product = Product.find(params[:id])
+  	@genres = Genre.all
+  	@cart_item = CartItem.new
   end
 
   def index
+  	@products = Product.where(sale_status: true).page(params[:page]).per(8)
+  	@genres = Genre.all
   end
+
 end
