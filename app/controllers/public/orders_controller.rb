6a6b5f44395617fc29fbb302e4aa6664ace1da9f
vagrant <vagrant@localhost.localdomain>
2020-08-19 08:06:39 +0000

    [Update] orders

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,4 +1,20 @@
 class Public::OrdersController < ApplicationController
-  def index
-  end
+	def index
+	end
+
+	def show
+	end
+
+	def new
+		@order = Order.new
+	end
+
+	def create
+	end
+
+	private
+	def order_params
+		params.require(:order).permit(:how_pay)
+	end
+
 end
