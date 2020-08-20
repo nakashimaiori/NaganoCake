06e1c15733c664a82eacbed9a93d208354a02e8e
vagrant <vagrant@localhost.localdomain>
2020-08-20 05:31:31 +0000

    [Add]seeds

---
+++ db/seeds/customer.rb
@@ -0,0 +1,16 @@
+  Customer.create!(
+    [
+      {
+      	id: 1,
+      	email: 'a@com',
+      	last_name: '中島', first_name: '伊織里',
+      	last_name_kana: 'ナカシマ', first_name_kana: 'イオリ',
+      	postal_code: '0000000',
+      	address: '大阪府大阪市難波ウェブキャンプ000-000',
+      	phone_number: '09011119999',
+      	customer_status: true,
+      	password: 'aaaaaa',
+      	password_confirmation: 'aaaaaa'
+      },
+    ]
+  	)
