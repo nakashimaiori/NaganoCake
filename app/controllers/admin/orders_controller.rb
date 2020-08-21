1e693d3bb2892d4e32aafcc58eb949be66b10738
vagrant <vagrant@localhost.localdomain>
2020-08-21 05:32:10 +0000

    [Add] admin/orders index

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,23 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.page(params[:page]).per(10)
   end
 
   def show
+    @order = Order.find(params[:id])
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path(@order.id)
+    else
+      render :show
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:receive_postal_code, :receive_address, :receive_name, :total_amount, :how_pay, :postage, :order_status)
   end
 end
