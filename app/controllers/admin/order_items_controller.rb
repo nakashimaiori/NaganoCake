e02b1df3b316e3242e8dde36a1c843cbd0d9531c
vagrant <vagrant@localhost.localdomain>
2020-08-21 08:02:56 +0000

    [Add] admin/order_items show

---
+++ app/controllers/admin/order_items_controller.rb
@@ -0,0 +1,12 @@
+class Admin::OrderItemsController < ApplicationController
+  def update
+    @order_item = OrderItem.find(params[:id])
+    @order_item.update(order_item_params)
+    redirect_to admin_order_path(@order_item.id)
+  end
+
+  private
+  def order_item_params
+    params.require(:order_item).permit(:made_status)
+  end
+end
