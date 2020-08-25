dda2f5c24a9400ba810a5fe09876ff2104c0015d
vagrant <vagrant@localhost.localdomain>
2020-08-25 06:29:40 +0000

    [Add]layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
 $(document).ready(function () {
   $("#theTarget").skippr({
     // スライドショーの変化 ("fade" or "slide")
-    transition : 'slide',
+    transition : 'fade',
     // 変化に係る時間(ミリ秒)
     speed : 1000,
     // easingの種類
@@ -56,11 +56,11 @@ $(document).ready(function () {
     // 子要素の種類("div" or "img")
     childrenElementType : 'div',
     // ナビゲーション矢印の表示(trueで表示)
-    arrows : true,
+    arrows : false,
     // スライドショーの自動再生(falseで自動再生なし)
     autoPlay : true,
     // 自動再生時のスライド切替間隔(ミリ秒)
-    autoPlayDuration : 2000,
+    autoPlayDuration : 2500,
     // キーボードの矢印キーによるスライド送りの設定(trueで有効)
     keyboardOnAlways : true,
     // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
