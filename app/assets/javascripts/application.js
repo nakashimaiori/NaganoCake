520594821964fe8a4cf51955cdf59bf4d67daa48
vagrant <vagrant@localhost.localdomain>
2020-08-29 05:45:08 +0000

    [Fix]prevew_top_details

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,30 +18,13 @@
 //= require bootstrap-sprockets
 
 
-// $( document ).on('turbolinks:load', function() {
-//   function readURL(input) {
-//     if (input.files && input.files[0]) {
-//       var reader = new FileReader();
-
-//       reader.onload = function (e) {
-//         $('#avatar_img_prev').attr('src', e.target.result);
-//       }
-//       reader.readAsDataURL(input.files[0]);
-//     }
-//   }
-
-//   $("#post_img").change(function(){
-//     $('#avatar_img_prev').removeClass('hidden');
-//     $('.avatar_present_img').remove();
-//     readURL(this);
-//   });
-// });
-
 
 $(function(){
     // inputのidから情報の取得
-    $('#item-image').on('change', function (e) {
+
+    $(document).on('change', '.image-prev', function (e) {
 // ここから既存の画像のurlの取得
+
     var reader = new FileReader();
     reader.onload = function (e) {
         $(".image").attr('src', e.target.result);
@@ -50,6 +33,7 @@ $(function(){
     reader.readAsDataURL(e.target.files[0]); //取得したurlにアップロード画像のurlを挿入
 });
 });
+
 
 
 $(document).ready(function () {
